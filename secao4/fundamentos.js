f8193b73f897595c1ad9839faf28869115ca7a41
gabrielPereira-ipcb <gsantana99.gs@gmail.com>
2024-12-04 12:52:53 +0000

    a

--- secao4/fundamentos.js
+++ secao4/fundamentos.js
@@ -1,12 +1,16 @@
-const cliente = {
-    nome: "Pedro",
-    idade: 32,
-    peso: 90,
-    endereco:{
-        logradouro:"rua Masd",
-        numero: 123
-    }
-}
+// const cliente = {
+//     nome: "Pedro",
+//     idade: 32,
+//     peso: 90,
+//     endereco:{
+//         logradouro:"rua Masd",
+//         numero: 123
+//     }
+// }
 
-console.log(cliente.nome);
-console.log(cliente.endereco.logradouro)
+// console.log(cliente.nome);
+// console.log(cliente.endereco.logradouro)
+
+for (let i = 1; i<=10; i++){
+    console.log(i)
+}
